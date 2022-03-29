fceaf11452902fdac404943ba47d75249a0167be
abdurrahman4127 <71093608+abdurrahman4127@users.noreply.github.com>
2022-03-29 23:23:37 +0600

    Update theoryClassFour_functionMethod.java

--- src/forMID/Lecture4_ForEachLoop_ClassObject/theoryClassFour_functionMethod.java
+++ src/forMID/Lecture4_ForEachLoop_ClassObject/theoryClassFour_functionMethod.java
@@ -1,16 +1,15 @@
-package forMID.Lecture4_ForEachLoop_ClassObject;//function means Method
+package forMID.Lecture4_ForEachLoop_ClassObject;
+//function means Method
 //Method : static returnType methodName (functionParameters)
 
-public class theoryClassFour_functionMethod
-{
-    static int sum(int a, int b) //mention 'static' if you want to write function inside 'main'
-    {
-        int c = a+b;
-        return (c);
+public class theoryClassFour_functionMethod{
+    public static void main(String[] args) {
+        System.out.println(sum(4, 10));
     }
-
-    public static void main(String[] args)
-    {
-        System.out.println(sum(4,10));
+    
+    //mention 'static' if you want to write function inside 'main'
+    static int sum(int a, int b) {
+        int c = a + b;
+        return (c);
     }
 }
