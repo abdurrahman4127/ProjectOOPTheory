e4fdf0d01c6241b1798174086f61b769a4d5ea6d
abdurrahman4127 <71093608+abdurrahman4127@users.noreply.github.com>
2022-03-29 23:25:58 +0600

    got rid of her name

--- src/forMID/Lecture4_ForEachLoop_ClassObject/theoryClassFour_classObject.java
+++ src/forMID/Lecture4_ForEachLoop_ClassObject/theoryClassFour_classObject.java
@@ -15,18 +15,18 @@ public class theoryClassFour_classObject {
 
         //className variableName = new className();
         Student ar7 = new Student();
-        Student nhi = new Student();
+        Student cr7 = new Student();
 
         //object name:
         ar7.name = "Abdur Rahman";
         ar7.cgpa = 3.77;
         ar7.id = "011202260";
-        nhi.name = "Thao Nhi"; //nhi.name = "Thao" + " Nhi"; both are same
-        nhi.cgpa = 3.7;
-        nhi.id = "011202260";
+        cr7.name = "Cristiano Ronaldo"; //cr7.name = "Cristiano" + " Ronaldo"; both are same
+        cr7.cgpa = 3.7;
+        cr7.id = "011202260";
 
         //printing ...
         System.out.println(ar7.name);
-        System.out.println(nhi.name);
+        System.out.println(cr7.name);
     }
 }
