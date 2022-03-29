7b2b2db5115e47a3a844907766d25a6740fdee12
abdurrahman4127 <71093608+abdurrahman4127@users.noreply.github.com>
2022-03-29 23:32:23 +0600

    got rid of her name

--- src/forFINAL/Nested_Class/AnonymousClass/AnonymousTest.java
+++ src/forFINAL/Nested_Class/AnonymousClass/AnonymousTest.java
@@ -1,49 +1,40 @@
 package forFINAL.Nested_Class.AnonymousClass;
 
-class Person
-{
+class Person{
     String name;
-    public Person(String a)
-    {
+    
+    public Person(String a){
         name = a;
     }
 
-    public void display()
-    {
+    public void display(){
         System.out.println("Hello from Person " + name);
     }
 
-    public void display(String msg)
-    {
-        System.out.println("Hello from Person " +name+ " " + msg);
+    public void display(String msg){
+        System.out.println("Hello from Person " + name + " " + msg);
     }
 }
 
-public class AnonymousTest
-{
+public class AnonymousTest{
     public static void main(String[] args)
     {
-        Person p = new Person("Thao Nhi")
+        Person p = new Person("L Lawliet")
         {
-
-            public void display()
-            {
+            public void display(){
                 display(2); // this is fine
                 System.out.println("Hello from Anonymous");
             }
 
             // Extra method
-            public void display(int a)
-            {
+            public void display(int a){
                 System.out.println("Number: " + a);
             }
-
         };
 
         p.display();
-        p.display("Vo"); // Ok
+        p.display("Hell"); // Ok
 //        p.display(3); // error: The method display(String) in the type Person is not applicable for the arguments (int)
 
     }
-
 }
